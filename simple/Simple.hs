deb53fb0f3e1d7c2c25e1693a07520802ed29225
George Singer <george.w.singer@gmail.com>
2018-07-12 08:29:34 -0400

    Rewrote Simple.hs and tweaked GDNLIB file to include OSX support.

--- simple/Simple.hs
+++ simple/Simple.hs
@@ -17,97 +17,56 @@ import Godot.Gdnative.Types
 
 import Foreign.C
 
+import Data.IORef
+import Data.Vector
 
+-- We encapsulate SIMPLE's member variables in the UserData type.
+data UserData = UserData
+                GodotObject    -- We must encode the actual instance of SIMPLE (analogous to C++'s `this`).
+                (IORef String) -- We use IORef to emulate OO state mutation.
+
+-- Then we emulate OO class inheritance: UserData inherits from GodotReference.
+instance HasBaseClass UserData where
+  type BaseClass UserData = GodotReference
+  super (UserData obj _) = GodotReference obj
 
 godot_gdnative_init :: GodotGdnativeInitOptionsPtr -> IO ()
 godot_gdnative_init optPtr = do
   putStrLn "gdnative init"
   opt <- peek optPtr
-  initApiStructs opt
+  initApiStructs opt -- We hide details involving `godot_gdnative_core_api_struct` and `godot_gdnative_ext_nativescript_api_struct`.
 
-foreign export ccall godot_gdnative_init :: GodotGdnativeInitOptionsPtr -> IO ()
-
+-- Question for David: How do we know if the  `godot_gdnative_core_api_struct` and `godot_gdnative_ext_nativescript_api_struct` are cleaned up? Does godot-haskell do this for us automatically?
 godot_gdnative_terminate :: GodotGdnativeTerminateOptionsPtr -> IO ()
 godot_gdnative_terminate optPtr = putStrLn "gdnative terminate"
-
-foreign export ccall godot_gdnative_terminate :: GodotGdnativeTerminateOptionsPtr -> IO ()
 
 godot_nativescript_init :: GdnativeHandle -> IO ()
 godot_nativescript_init desc = do
   putStrLn "nativescript init"
-  outStr <- (toLowLevel (T.pack "i did a thing")) :: IO GodotString
-  lol <- (toLowLevel (VariantString outStr) :: IO GodotVariant)
+  -- Below we use simplified wrapper functions for `godot_nativescript_register_class` and `godot_nativescript_register_method`.
+  registerClass desc "SIMPLE" "Reference" simple_constructor simple_destructor
+  registerMethod desc "SIMPLE" "get_data" GodotMethodRpcModeDisabled simple_get_data
+
+simple_constructor :: GodotObject -> IO UserData
+simple_constructor obj = do
+   putStrLn "SIMPLE._init()"
+   stringRef <- newIORef "World from GDNative!"
+   return (UserData obj stringRef)
+
+simple_destructor :: GodotObject -> UserData -> IO ()
+simple_destructor obj userData = do
+  putStrLn "SIMPLE._byebye()"
   return ()
-  -- toLowLevel (VariantString outStr)
-  -- Replace with SIMPLE class
-  -- registerClass desc "TestClass" "Node" (\obj -> return (TestClass obj (show obj))) (\_ _ -> return ())
-  -- registerMethod desc "TestClass" "do_a_thing" GodotMethodRpcModeDisabled $
-  --   \_ t@(TestClass obj str) _ -> do
-  --     putStrLn str
-      
-  --     str <- Godot.get_class t >>= fromLowLevel
-  --     putStr "Godot.get_class is "
-  --     putStrLn $ T.unpack str
 
-  --     outStr <- toLowLevel (T.pack "i did a thing")
-  --     (toLowLevel (VariantString outStr) :: IO GodotVariant)
+-- Question: What happens to the GodotString? In the C code we had to manually delete it via `godot_string_destroy`. (What about GodotVariants)?
+simple_get_data :: GodotObject -> UserData -> Vector GodotVariant -> IO GodotVariant
+simple_get_data _ t@(UserData obj ioRefStr) _ = do
+  userDataStr <- readIORef ioRefStr
+  userDataGodotStr <- toLowLevel $ T.pack userDataStr                 -- Convert our String to a GodotString via toLowLevel.
+  userDataGodotVariant <- toLowLevel (VariantString userDataGodotStr) -- Convert our GodotString into a GodotVariant via toLowLevel.
+  return userDataGodotVariant
 
-
+-- We finally export the core godot_* functions so that NativeScript can call them automatically when instances of our SIMPLE script are loaded.
+foreign export ccall godot_gdnative_init :: GodotGdnativeInitOptionsPtr -> IO ()
+foreign export ccall godot_gdnative_terminate :: GodotGdnativeTerminateOptionsPtr -> IO ()
 foreign export ccall godot_nativescript_init :: GdnativeHandle -> IO ()
-
-data TestClass = TestClass GodotObject String
-  deriving (Show, Eq)
-
-instance HasBaseClass TestClass where
-  type BaseClass TestClass = GodotReference
-  super (TestClass obj _) = GodotReference obj
-
-
-{- Dummy Data -}
-
-data UserData
--- typedef struct user_data_struct {
--- 	char data[256];
--- } user_data_struct;
-
-data MethodData
-
-simple_constructor :: GodotObject -> MethodData -> UserData -> Int -> GodotVariant -> IO ()
-simple_constructor inst methodData = undefined
-
-simple_destructor :: GodotObject -> MethodData -> UserData -> IO()
-simple_destructor inst methodData userData = undefined
-
--- type InstanceMethodFun = Ptr GodotVariant -> GodotObject -> Ptr () -> Ptr () -> CInt -> Ptr (Ptr GodotVariant) -> IO (Ptr GodotVariant)
--- simple_get_data :: InstanceMethodFun
-{-
- godot_variant simple_get_data(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
-	godot_string data;
-	godot_variant ret;
-	user_data_struct * user_data = (user_data_struct *) p_user_data;
-
-	api->godot_string_new(&data);
-	api->godot_string_parse_utf8(&data, user_data->data);
-	api->godot_variant_new_string(&ret, &data);
-	api->godot_string_destroy(&data);
-
-	return ret;
-} 
--}
-simple_get_data :: Ptr GodotVariant -> GodotObject -> Ptr () -> Ptr () -> CInt -> Ptr (Ptr GodotVariant) -> IO (Ptr GodotVariant) 
-simple_get_data returnPtr instObj ptrMethodData ptrUserData numArgs listOfArgs = do
-   userData <- peek ptrUserData
-
-   -- Q: How does one cast a String (into a GodotString) into a GodotVariant (with memory cleanup, as below)?
-   -- godot_string data;
-   -- godot_variant ret;
-   -- user_data_struct * user_data = (user_data_struct *) p_user_data;
-   -- api->godot_string_new(&data);
-   -- api->godot_string_parse_utf8(&data, user_data->data);
-   -- api->godot_variant_new_string(&ret, &data);
-   -- api->godot_string_destroy(&data);
-
-   outStr <- (toLowLevel (T.pack "i did a thing")) :: IO GodotString
-   toLowLevel (VariantString outStr)
-
-   return returnPtr
