71b1277315a9e090b909cf70523a0e92427089ee
George Singer <george.w.singer@gmail.com>
2018-07-12 09:05:19 -0400

    Fixed memory leak.

--- simple/Simple.hs
+++ simple/Simple.hs
@@ -36,14 +36,14 @@ godot_gdnative_init optPtr = do
   opt <- peek optPtr
   initApiStructs opt -- We hide details involving `godot_gdnative_core_api_struct` and `godot_gdnative_ext_nativescript_api_struct`.
 
--- Question for David: How do we know if the  `godot_gdnative_core_api_struct` and `godot_gdnative_ext_nativescript_api_struct` are cleaned up? Does godot-haskell do this for us automatically?
 godot_gdnative_terminate :: GodotGdnativeTerminateOptionsPtr -> IO ()
 godot_gdnative_terminate optPtr = putStrLn "gdnative terminate"
 
 godot_nativescript_init :: GdnativeHandle -> IO ()
 godot_nativescript_init desc = do
   putStrLn "nativescript init"
-  -- Below we use simplified wrapper functions for `godot_nativescript_register_class` and `godot_nativescript_register_method`.
+  -- Below we use simplified wrapper functions for `godot_nativescript_register_class`
+  -- and `godot_nativescript_register_method`.
   registerClass desc "SIMPLE" "Reference" simple_constructor simple_destructor
   registerMethod desc "SIMPLE" "get_data" GodotMethodRpcModeDisabled simple_get_data
 
@@ -58,15 +58,16 @@ simple_destructor obj userData = do
   putStrLn "SIMPLE._byebye()"
   return ()
 
--- Question: What happens to the GodotString? In the C code we had to manually delete it via `godot_string_destroy`. (What about GodotVariants)?
 simple_get_data :: GodotObject -> UserData -> Vector GodotVariant -> IO GodotVariant
 simple_get_data _ t@(UserData obj ioRefStr) _ = do
   userDataStr <- readIORef ioRefStr
   userDataGodotStr <- toLowLevel $ T.pack userDataStr                 -- Convert our String to a GodotString via toLowLevel.
   userDataGodotVariant <- toLowLevel (VariantString userDataGodotStr) -- Convert our GodotString into a GodotVariant via toLowLevel.
-  return userDataGodotVariant
+  godot_string_destroy userDataGodotStr                               -- We must still destroy the GodotString, just as we would in C.
+  return userDataGodotVariant                                         -- The GodoVariant we return is still destroyed automatically by Godot.
 
--- We finally export the core godot_* functions so that NativeScript can call them automatically when instances of our SIMPLE script are loaded.
+-- We finally export the core godot_* functions, so that NativeScript can call
+-- them automatically when instances of our SIMPLE script are loaded.
 foreign export ccall godot_gdnative_init :: GodotGdnativeInitOptionsPtr -> IO ()
 foreign export ccall godot_gdnative_terminate :: GodotGdnativeTerminateOptionsPtr -> IO ()
 foreign export ccall godot_nativescript_init :: GdnativeHandle -> IO ()
